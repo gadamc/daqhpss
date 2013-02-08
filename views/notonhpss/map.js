48c4fb02e43471cad64d9da904e0e3bc801daf50
gadamc <adam.cox@kit.edu>
2013-02-07 18:27:50 -0800

    woops... now its a valid function

--- views/notonhpss/map.js
+++ views/notonhpss/map.js
@@ -1,15 +1,15 @@
 function(doc) {
   if(doc.type == "daqdocument" && doc.status == "good" && 
-  !doc.hpss && doc.proc0 && doc['proc0']['file_size'] 
-  && doc.run_name >= "mj"){
+    !doc.hpss && doc.proc0 && doc['proc0']['file_size'] 
+    && doc.run_name >= "mj"){
 
-  emit( doc.run_name, doc['proc0']['file_size']);
-    }
+    emit( doc.run_name, doc['proc0']['file_size']);
+  }
 
   if(doc.type == "daqmetadatadocuments" && doc.status == "good" && 
-  !doc.hpss && doc.metaproc0 && doc['metaproc0']['file_size'] 
-  && doc.run_name >= "mj"){
+    !doc.hpss && doc.metaproc0 && doc['metaproc0']['file_size'] 
+    && doc.run_name >= "mj"){
 
-  emit( doc.run_name, doc['metaproc0']['file_size']);
-    }
+    emit( doc.run_name, doc['metaproc0']['file_size']);
+  }
 }
--- views/tarfiles/map.js
+++ views/tarfiles/map.js
@@ -1,6 +1,5 @@
 function(doc) {
-  if( (doc.type == "hpsstarfiledocument" && doc['date_unixtime'] && doc['file_size']){
-
+  if( doc.type == "hpsstarfiledocument" && doc['date_unixtime'] && doc['file_size']){
       emit( doc['date_unixtime'], doc['file_size']);
     }
 }
