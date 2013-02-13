7bf0041c23fa55a9837d6e2e4025a827b9215c6c
gadamc <adam.cox@kit.edu>
2013-02-13 07:21:57 -0800

    changing onhpss to match view result format as notonhpss. this also matches other proc view result structures, allowing the use of some KDataPy scripts such as removeProc and resetStatus.

--- views/onhpss/map.js
+++ views/onhpss/map.js
@@ -3,13 +3,13 @@ function(doc) {
     doc['hpss']['file'] && doc.proc0 && doc['proc0']['file_size'] 
     && doc.run_name >= "mj"){
 
-    emit( [doc.run_name, doc['hpss']['file']], doc['proc0']['file_size']);
+    emit( doc.run_name, doc['proc0']['file_size']);
   }
 
   if(doc.type == "daqmetadatadocuments"  && 
     doc['hpss']['file'] && doc.metaproc0 && doc['metaproc0']['file_size'] 
     && doc.run_name >= "mj"){
 
-    emit( [doc.run_name, doc['hpss']['file']], doc['metaproc0']['file_size']);
+    emit( doc.run_name, doc['metaproc0']['file_size']);
   }
 }
