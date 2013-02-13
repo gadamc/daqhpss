4023c354b1d693400d65f9fa3d7314d73dfe4f7b
gadamc <adam.cox@kit.edu>
2013-02-12 19:12:58 -0800

    adding hpss in progress view

--- views/inprogress/map.js
+++ views/inprogress/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   if( (doc.type == "daqdocument" || doc.type == "daqmetadatadocuments")  && 
-        doc.['status'] == 'hpss in progress' && doc.run_name ){
+        doc.['status'] == "hpss in progress" && doc.run_name ){
 
       emit( doc.run_name,  1);
     }
