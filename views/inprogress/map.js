348e2800c2ed8eb22342fb421a24251da640a452
gadamc <adam.cox@kit.edu>
2013-02-14 14:36:18 -0800

    removing unnecessary views. fixing views to match spsToHpss.py script in kdata

--- views/inprogress/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-  if( (doc.type == "daqdocument" || doc.type == "daqmetadatadocuments")  && 
-        doc["status"] == "hpss in progress" && doc.run_name ){
-
-      emit( doc.run_name,  1);
-    }
-}
--- views/notonhpss/map.js
+++ views/notonhpss/map.js
@@ -1,14 +1,16 @@
 function(doc) {
+
+
   if(doc.type == "daqdocument"  && 
-    !doc.hpss && doc.proc0 && doc['proc0']['file_size'] 
-    && doc.run_name >= "mj"){
+    (doc.hpss_status != "hpss in progress" || !doc.hpss_status) && !doc.hpss && doc.run_name >= "mj" &&
+    doc.proc0 && doc['proc0']['file_size']){
 
     emit( doc.run_name, doc['proc0']['file_size']);
   }
 
-  if(doc.type == "daqmetadatadocuments"  && 
-    !doc.hpss && doc.metaproc0 && doc['metaproc0']['file_size'] 
-    && doc.run_name >= "mj"){
+  if(doc.type == "daqmetadatadocuments"  &&
+    (doc.hpss_status != "hpss in progress" || !doc.hpss_status) && !doc.hpss && doc.run_name >= "mj" &&
+    doc.metaproc0 && doc['metaproc0']['file_size'] ){
 
     emit( doc.run_name, doc['metaproc0']['file_size']);
   }
