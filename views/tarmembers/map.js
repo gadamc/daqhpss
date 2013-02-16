d16c4741dfd07dc0a74fac5a4d91b4f0c445502e
gadamc <adam.cox@kit.edu>
2013-02-15 16:23:50 -0800

    using built-in isArray function

--- views/tarmembers/map.js
+++ views/tarmembers/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if( doc.type == "hpsstarfiledocument"  && doc.members && doc.file && Object.prototype.toString.call(doc.members) === '[object Array]'){
+  if( doc.type == "hpsstarfiledocument"  && doc.members && doc.file && isArray(doc.members)){
 
       for(var i in doc.members){
         emit( doc['members'][i], doc.file);
